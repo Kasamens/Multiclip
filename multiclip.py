a305484eac051a69f2b1c3c6507a173fc7beda92
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-02-12 12:03:18 +0000

    Added delete functionality

--- multiclip.py
+++ multiclip.py
@@ -1,32 +1,64 @@
 import sys
+
+from numpy import save
 import clipboard
 import json
+
+# This is the name of the file to store data
+SAVED_DATA = "clipboard.json"
 
 # This function saves commands in a json file
 
 
-def save_items(filepath, data):
+def save_data(filepath, data):
     with open(filepath, "w") as f:
         json.dump(data, f)
 
 # This function reads commands from a json file
 
 
-def load_items(filepath):
-    with open(filepath, "r") as f:
-        data = json.load(f)
-        return data
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
 
 
 if(len(sys.argv) == 2):
     command = sys.argv[1]
+    # This copies all the commands in the file into the variable
+    data = load_data(SAVED_DATA)
 
     if command == "save":
-        pass
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+
+        # This apppends the new item to the dictionary
+        # and overwrites the clipboard file with the data
+        save_data(SAVED_DATA, data)
+        print("Data saved")
+
     elif command == "load":
-        pass
+        key = input("Enter a key: ")
+        if key not in data:
+            print("Key does not exist")
+        else:
+            clipboard.copy(data[key])
+            print("Data copied to clipboard")
+
     elif command == "list":
-        pass
+        print(data)
+
+    elif command == "delete":
+        key = input("Enter a key: ")
+        if key not in data:
+            print("Key does not exist")
+        else:
+            data.pop(key)
+            print(f"{key} deleted successfully")
+
     else:
         print("Unknown Command")
 else:
