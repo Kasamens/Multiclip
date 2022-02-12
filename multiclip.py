061c2c8e2a544df2adaca6a46631fe5eb760616c
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-02-12 11:41:59 +0000

    Added save and load functions

--- multiclip.py
+++ multiclip.py
@@ -2,6 +2,32 @@ import sys
 import clipboard
 import json
 
-# Stores text from clipboard in variable
-data = clipboard.paste()
-print(data)
+# This function saves commands in a json file
+
+
+def save_items(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+# This function reads commands from a json file
+
+
+def load_items(filepath):
+    with open(filepath, "r") as f:
+        data = json.load(f)
+        return data
+
+
+if(len(sys.argv) == 2):
+    command = sys.argv[1]
+
+    if command == "save":
+        pass
+    elif command == "load":
+        pass
+    elif command == "list":
+        pass
+    else:
+        print("Unknown Command")
+else:
+    print("Please pass exactly one command")
